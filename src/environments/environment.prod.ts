d036f383e0a2281707fdf05058981435bf75d238
Waqar Ali <mu.waqar.ali@gmail.com>
2021-01-21 18:16:20 +0500

    fix issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
     production: true,
     hmr: false,
-    API_URL: 'https://public-api.366dev.nl',
-    REGION: 'eu-central-1',
-    BUCKET: '366dev01',
-    ACCESS_ID: 'AKIASISFIGT234K5HRFG',
-    ACCESS_KEY: 'udz18M3gVIDCl6GEhVw4tccb8PWS9gukIjRYXK0e'
+    API_URL: 'https://public-api.366dev.nl'
 };
